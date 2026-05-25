4f50c4d6d5b0c39a618cf83df2aaff192c65a3c2
Sowmya Chilpa <sowmya.chilpa@techdenali.com>
2026-05-25 16:01:35 +0530

    added forgot password functionality

--- travel-backend/controllers/authController.js
+++ travel-backend/controllers/authController.js
@@ -1,55 +1,69 @@
-const {
-  registerService,
-  loginService,
-} = require("../services/authService");
+const { registerService, loginService, forgotPasswordService, resetPasswordService, } = require("../services/authService");
 
 const registerUser = async (req, res) => {
-
   try {
-
     const user = await registerService(
       req.body
     );
-
     res.status(201).json({
       message: "User Registered Successfully",
       user,
     });
-
   } catch (error) {
-
     res.status(400).json({
       message: error.message,
     });
-
   }
-
 };
 
 const loginUser = async (req, res) => {
-
   try {
-
     const result = await loginService(
       req.body
     );
-
     res.status(200).json({
       message: "Login Successful",
       ...result,
     });
-
   } catch (error) {
-
-    res.status(500).json({
+    res.status(400).json({
       message: error.message,
     });
-
   }
+};
 
+const forgotPassword = async (req, res) => {
+  try {
+    const result =
+      await forgotPasswordService(
+        req.body.email
+      );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const result =
+      await resetPasswordService(
+        req.params.token,
+        req.body.password
+      );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
   registerUser,
   loginUser,
+  forgotPassword,
+  resetPassword,
 };
